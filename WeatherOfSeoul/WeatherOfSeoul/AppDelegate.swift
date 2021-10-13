d41e62c9c1342e1900f2725a1231515a99369bf7
권준상 <wnstkdsun97@naver.com>
2021-10-13 15:47:17 +0900

    Week5 네이버 로그인

--- WeatherOfSeoul/WeatherOfSeoul/AppDelegate.swift
+++ WeatherOfSeoul/WeatherOfSeoul/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import KakaoSDKCommon
+import NaverThirdPartyLogin
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,9 +16,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         KakaoSDKCommon.initSDK(appKey: "4f5e5aa2cc727eb182b467d9126288e8")
+        
+        let instance = NaverThirdPartyLoginConnection.getSharedInstance()
+        
+        instance?.isNaverAppOauthEnable = false
+        instance?.isInAppOauthEnable = true
+        instance?.isOnlyPortraitSupportedInIphone()
+        
+        // 네이버 아이디로 로그인하기 설정
+        instance?.serviceUrlScheme = kServiceAppUrlScheme
+        instance?.consumerKey = kConsumerKey
+        instance?.consumerSecret = kConsumerSecret
+        instance?.appName = kServiceAppName
         return true
     }
 
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        NaverThirdPartyLoginConnection.getSharedInstance()?.application(app, open: url, options: options)
+            return true
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- WeatherOfSeoul/WeatherOfSeoul/SignInViewController.swift
+++ WeatherOfSeoul/WeatherOfSeoul/SignInViewController.swift
@@ -8,16 +8,19 @@
 import UIKit
 import KakaoSDKAuth
 import KakaoSDKUser
+import NaverThirdPartyLogin
 
 class SignInViewController: UIViewController {
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let loginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loginInstance?.delegate = self
     }
     
-    @IBAction func signInButtonDIdTap(_ sender: Any) {
+    @IBAction func kakaoSignInButtonDidTap(_ sender: Any) {
         UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
            if let error = error {
              print(error)
@@ -35,5 +38,35 @@ class SignInViewController: UIViewController {
         }
     }
     
-
+    @IBAction func naverSignInButtonDidTap(_ sender: Any) {
+        loginInstance?.requestThirdPartyLogin()
+    }
+    
 }
+
+extension SignInViewController: NaverThirdPartyLoginConnectionDelegate {
+    
+    func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+        print("Success Naver login")
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let weatherVC = storyboard.instantiateViewController(withIdentifier: "WeatherVC")
+        weatherVC.modalPresentationStyle = .fullScreen
+
+        self.present(weatherVC, animated: false, completion: nil)
+    }
+
+    func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+        loginInstance?.accessToken
+    }
+
+    func oauth20ConnectionDidFinishDeleteToken() {
+        print("log out")
+    }
+
+    func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
+        print(error.localizedDescription)
+    }
+}
+
+
+
